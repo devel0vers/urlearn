f63d2cfa269978d5d1c184c2bcbd12e9c50d0267
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2020-11-25 13:37:22 +0700

    update owner file

---
+++ docker/reload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# goto basedir
+cd $(dirname "$0")
+
+mkdir data/mysql -p
+mkdir logs/apache2 -p
+mkdir logs/mysql -p
+
+docker-compose pull
+docker-compose stop
+docker-compose down
+docker-compose up --build -d 
