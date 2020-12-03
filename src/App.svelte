9ba8dda99d5fa22191e4cf10c74362cdd1e3d7ae
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2020-12-03 10:17:52 +0700

    fix env svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<h1>Hello Hello {name}!</h1>
+	<h1>sHello Hello {name}!</h1>asd
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
